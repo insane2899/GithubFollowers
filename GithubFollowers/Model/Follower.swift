bbd1134dec2d111a19edf215dbda20137541d6e1
Soumik Sen <imrimosen@gmail.com>
2024-05-19 13:39:50 +0530

    Created Models

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,15 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Soumik Sen on 19/05/24.
+//
+
+import Foundation
+
+// MARK: We can have names as camelcase and Codable will handle that. EG. avatar_url can be named as avatarUrl
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
